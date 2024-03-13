4ef814383516efd3b9e94dadfd5719b036f4985d
Rofleksey <evilmorty@yandex.ru>
2024-03-13 18:56:52 +0300

    Handle non-0 exit codes

--- pilot.js
+++ pilot.js
@@ -21,13 +21,18 @@ async function executePilotTask(args) {
     } catch (ignored) {
     }
 
-    console.log(`Executing sb_pilot with args ${args}...`);
-    const result = await execa(PILOT_EXECUTABLE_PATH, args, {
-      all: true,
-    });
-    console.log(`Executed sb_pilot with args ${args}: ${result}`);
+    let all, message, exitCode;
 
-    const {all, message, exitCode} = result;
+    console.log(`Executing sb_pilot with args ${args}...`);
+    try {
+      const result = await execa(PILOT_EXECUTABLE_PATH, args, {
+        all: true,
+      });
+      ({all, message, exitCode} = result);
+    } catch (e) {
+      ;({all, message, exitCode} = e);
+    }
+    console.log(`Executed sb_pilot with args ${args} with exit code ${exitCode}, awaiting result...`);
 
     await waitForFiles([PILOT_E_FILE_PATH, PILOT_P_FILE_PATH], {
       timeout: 300000,
@@ -48,10 +53,12 @@ async function executePilotTask(args) {
     } catch (ignored) {
     }
 
+    console.log(`Got result for sb_pilot call with args ${args}: eText=${eText} pText=${pText}`);
+
     return {
       eText,
       pText,
-      output: all?.toString() ?? message?.toString(),
+      output: all?.toString() || message?.toString(),
       exitCode
     }
   } catch (e) {
