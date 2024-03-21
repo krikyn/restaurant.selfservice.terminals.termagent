e30ec372c5c15e508ef22d617b575aed09cdcb04
Rofleksey <evilmorty@yandex.ru>
2024-03-21 22:20:57 +0300

    Enable auto updates

--- consts.js
+++ consts.js
@@ -8,6 +8,7 @@ export const API_URL = process.env.API_URL || "http://localhost:8080";
 export const BASE_WS_URL = process.env.SERVER_URL || "ws://localhost:8080/ws";
 export const USERNAME = process.env.WS_USERNAME || "test";
 export const PASSWORD = process.env.WS_PASSWORD || "test";
+export const UPDATE_INTERVAL = Number(process.env.UPDATE_INTERVAL || "3600000");
 export const AGENT_DEBUG = !process.env.AGENT_DEBUG;
 export const PILOT_FOLDER_PATH = path.resolve(process.env.PILOT_FOLDER_PATH || ".");
 export const PILOT_EXECUTABLE_PATH = path.join(PILOT_FOLDER_PATH, "sb_pilot.exe");
